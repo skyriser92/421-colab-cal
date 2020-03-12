0a4864a33a22ec5084814b4869b746347dcd9f5a
jscottsiri <jss82@njit.edu>
2020-03-12 15:36:47 -0400

    Add Repo

---
+++ IS421GroupProject/IS421GroupProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IS421GroupProject
+{
+    class Program
+    {
+        public static int RandInt(int range1, int range2)
+        {
+            Random rand = new Random();
+            return rand.Next(range1, range2);
+        }
+    }
+}
